f222f9bb428c8018218e6571dc1988260d28da14
cheshi-mantu <cheshi.mantu@gmail.com>
2020-05-21 18:13:07 +0300

    commit before Page Object works

---
+++ facebookTests/src/test/pages/FacebookPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+
+
