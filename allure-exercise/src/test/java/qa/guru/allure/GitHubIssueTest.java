abacf9c3301d2b5e5693aeb1ec249afd4e73c20c
cheshi-mantu <cheshi.mantu@gmail.com>
2020-05-29 21:46:55 +0300

    added selenide listener

--- allure-exercise/src/test/java/qa/guru/allure/GitHubIssueTest.java
+++ allure-exercise/src/test/java/qa/guru/allure/GitHubIssueTest.java
@@ -1,25 +1,52 @@
 package qa.guru.allure;
 
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Feature("Work with issues")
 public class GitHubIssueTest {
+    private final static int ISSUE_NUMBER = 1;
+
+    @Before // <= this is JUnit notation
+    //Adding listener of selenide before all tests execution
+    public void initSelenideListener () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
     @Test
     @Story("Check if issues do exist")
     @DisplayName("Allure Example - issue must exists in github repository")
     public void testIssueExists() {
-        step("1. Open main page");
-        step("2. Authentication as user");
-        step("3. Open user's homepage with repo");
-        step("4. Open user's page with issues");
-        step("5. Open check if issue with ID=12 exists");
+        step("1. Open main page", () -> {
+            open("https://github.com");
+        });
+        //step("2. Authentication as user");
+        step("3. Open user's homepage with repo", ()-> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("cheshi_mantu/qa-guru");
+            $(".header-search-input").submit();
+            $(By.linkText("cheshi-mantu/qa-guru")).shouldHave(Condition.visible);
+            $(By.linkText("cheshi-mantu/qa-guru")).click();
+        });
+        step("4. Open user's page with issues", ()-> {
+            $(withText("Issues")).click();
+        });
+        step("5. Open check if we have issue with ID=" + ISSUE_NUMBER, ()-> {
+            $(withText("#"+ISSUE_NUMBER)).exists();
+        });
+
 
 
     }
