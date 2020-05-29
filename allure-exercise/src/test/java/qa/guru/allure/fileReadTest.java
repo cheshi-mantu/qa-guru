424fece9138ac6018dc6834fb32331bdb3b7beb5
cheshi-mantu <cheshi.mantu@gmail.com>
2020-05-29 23:08:30 +0300

    created file reader helper to read secret tokens commit before github client

---
+++ allure-exercise/src/test/java/qa/guru/allure/fileReadTest.java
@@ -0,0 +1,25 @@
+package qa.guru.allure;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@Feature("Work with Projects")
+public class fileReadTest {
+        private final fileReadHelper txtFile = new fileReadHelper();
+        @Test
+        @Story("Check reading from file")
+        @DisplayName("This checks if string returned from file is correct")
+        public void testString(){
+                String txtToken = "";
+                txtToken = txtFile.getStringFromFile("D:\\code\\qa-guru\\allure-exercise\\src\\test\\java\\qa\\guru\\allure\\token.txt");
+                System.out.println("This is what we got:" + txtToken);
+                assertEquals("AAAAAAAAABBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDD", txtToken);
+        }
+
+
+}
